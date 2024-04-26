6b5de573952caf1535b2e91407d7cbb1e8d3a475
Jan Sevcik <jan.sevcik@student.gyarab.cz>
2024-04-26 09:45:48 +0200

    1

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from filmy.models import Movie
+from filmy.models import Movie, Director, Genre
 
 admin.site.register(Movie)
+admin.site.register(Director)
+admin.site.register(Genre)
 
 # Register your models here.
--- filmy/models.py
+++ filmy/models.py
@@ -3,12 +3,36 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    title = models.CharField(max_length=300)
+    name = models.CharField(max_length=300)
     year = models.IntegerField(blank=True, null=True)
+    footage = models.PositiveSmallIntegerField(blank=True, null=True)
 
     def __str__(self):
-        return self.title
+        return self.name
     
     class Meta:
         verbose_name = 'Film'
         verbose_name_plural = 'Filmy'
+
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+    birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    main_picture = models.ImageField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        verbose_name = 'Director'
+        verbose_name_plural = 'Directors'
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
