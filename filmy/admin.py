e9ada753082feb429134ed64681907b9d6bf3b6b
Jan Sevcik <jan.sevcik@student.gyarab.cz>
2024-04-26 09:45:43 +0200

    1

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from filmy.models import Movie
+
+admin.site.register(Movie)
+
 # Register your models here.
--- filmy/models.py
+++ filmy/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=300)
+    year = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Film'
+        verbose_name_plural = 'Filmy'
