c56678edefcbcb65e95534afdb74f4988ee9cf4c
Jan Sevcik <jan.sevcik@student.gyarab.cz>
2024-05-31 07:48:17 +0200

    test

--- filmy/admin.py
+++ filmy/admin.py
@@ -2,9 +2,27 @@ from django.contrib import admin
 
 from filmy.models import Movie, Director, Genre, Actor
 
-admin.site.register(Movie)
-admin.site.register(Director)
-admin.site.register(Genre)
-admin.site.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    pass
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'year', 'genres_display', 'director']
+    list_display_links = ['name', 'id']
+    search_fields = ['name', 'director__name']
+    list_filter = ['genres', 'director']
+
+class DirectorAdmin(admin.ModelAdmin):
+    pass
+
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Movie, MovieAdmin)
+
+admin.site.register(Director, DirectorAdmin)
+
+admin.site.register(Genre, GenreAdmin)
+
+admin.site.register(Actor, ActorAdmin)
 
 # Register your models here.
